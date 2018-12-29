6f1d5a3044cff8e953a5f7133602621a5250245b
annajjj <ajonak@o2.pl>
2018-12-29 22:53:20 +0100

    pierwszy algorytm prawie ok :)

--- ania.js
+++ ania.js
@@ -2,6 +2,50 @@
 exports.__esModule = true;
 var mock_1 = require("./mock");
 var cTable = require("console.table");
+function checkPlace(container, array) {
+    var placedContainers = new Array();
+    placedContainers = JSON.parse(JSON.stringify(array));
+    var result = checkFreePlace(container, placedContainers, 'normal');
+    if (!result.valid)
+        result = checkFreePlace(container, placedContainers, 'rotated');
+    return result;
+}
+function checkFreePlace(container, array, type) {
+    var _a;
+    var placedContainers = new Array();
+    placedContainers = JSON.parse(JSON.stringify(array));
+    if (type == 'rotated')
+        _a = [container.length, container.width], container.width = _a[0], container.length = _a[1];
+    for (var shipY = 0; shipY < mock_1.ship.width; shipY++) {
+        for (var shipX = 0; shipX < mock_1.ship.length; shipX++) {
+            if (placedContainers[shipY][shipX] === 'x') {
+                console.log('s', shipY, shipX);
+                for (var containerY = shipY; containerY < shipY + container.width; containerY++) {
+                    for (var containerX = shipX; containerX < shipX + container.length; containerX++) {
+                        if (placedContainers[containerY][containerX] !== 'x') {
+                            containerY = shipY + container.width;
+                            containerX = shipX + container.length;
+                            console.log('oj');
+                        }
+                        if ((container.length == (containerX - shipX + 1)) && (container.width == (containerY - shipY + 1))) {
+                            // console.log(container.length, containerX, shipX, container.Y, shipY)
+                            var filledArray = fillArray(placedContainers, shipY, shipX, container);
+                            return { placedContainers: filledArray, valid: true };
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return { valid: false };
+}
+function fillArray(array, x, y, container) {
+    console.log(container, x, y);
+    for (var i = x; i < container.width + x; i++)
+        for (var j = y; j < container.length + y; j++)
+            array[i][j] = container.id;
+    return array;
+}
 var sortedContainers = mock_1.containers
     .sort(function (a, b) {
     if (a.timestamp > b.timestamp)
@@ -11,25 +55,23 @@ var sortedContainers = mock_1.containers
     else
         return (a.width * a.length < b.width * b.length) ? 1 : -1;
 });
-// console.log(sortedContainers)
 var placedContainers = new Array(mock_1.ship.length);
 for (var i = 0; i < mock_1.ship.length; i++) {
     placedContainers[i] = new Array(mock_1.ship.width);
     for (var j = 0; j < mock_1.ship.width; j++)
         placedContainers[i][j] = 'x';
 }
-//console.log("placed", placedContainers[0])
-//  let x = [[]];
-//  x[0] = [2,1,4];
-//  x[1] = [3,5,5]
-// console.log(x[1][1])
-// console.log(placedContainers[0][1])
-if (mock_1.ship.length >= sortedContainers[0].length) {
-    if (mock_1.ship.width >= sortedContainers[0].width) {
-        for (var i = 0; i < sortedContainers[0].length; i++)
-            for (var j = 0; j < sortedContainers[0].width; j++)
-                placedContainers[i][j] = sortedContainers[0].id;
-    }
-}
+// let result = checkPlace(sortedContainers[0], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
+// result = checkPlace(sortedContainers[1], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
+// result = checkPlace(sortedContainers[2], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
+sortedContainers.forEach(function (el) {
+    var result = checkPlace(el, placedContainers);
+    if (result.valid)
+        placedContainers = result.placedContainers;
+});
 var table = cTable.getTable(placedContainers);
 console.table(placedContainers);
+console.log(sortedContainers);
--- ania.ts
+++ ania.ts
@@ -1,6 +1,53 @@
 import { containers, ship } from "./mock";
 import * as cTable from 'console.table';
 
+function checkPlace(container, array){
+    let placedContainers = new Array();
+    placedContainers = JSON.parse(JSON.stringify(array));
+    let result = checkFreePlace(container, placedContainers, 'normal');
+    if(!result.valid) result = checkFreePlace(container, placedContainers, 'rotated');
+    return result 
+}
+
+function checkFreePlace(container, array, type: 'normal' | 'rotated' ){
+    let placedContainers = new Array();
+    placedContainers = JSON.parse(JSON.stringify(array));
+
+    if (type == 'rotated') [container.width, container.length] = [container.length, container.width]; 
+    for(let shipY = 0; shipY < ship.width; shipY++){
+        for (let shipX = 0; shipX <ship.length; shipX++){
+            if(placedContainers[shipY][shipX]==='x'){
+                console.log('s', shipY, shipX)
+                for(let containerY=shipY; containerY < shipY + container.width; containerY++){
+                    for(let containerX=shipX; containerX < shipX + container.length; containerX++){
+                        if(placedContainers[containerY][containerX] !== 'x') {
+                            containerY= shipY + container.width;
+                            containerX= shipX + container.length;
+                            console.log('oj')
+                        }
+                        if((container.length == (containerX - shipX + 1)) && (container.width == (containerY - shipY + 1)))
+                        {
+                            // console.log(container.length, containerX, shipX, container.Y, shipY)
+                            const filledArray = fillArray(placedContainers, shipY, shipX, container)
+                            return {placedContainers: filledArray, valid: true};
+                        }
+            }
+        }
+    }
+    }
+}
+return {valid: false};
+
+}
+
+function fillArray(array, x, y, container){
+    console.log(container, x, y)
+    for(let i=x; i<container.width+x; i++)
+        for(let j=y; j<container.length+y; j++)
+            array[i][j] = container.id;
+    
+    return array;
+}
 
 const sortedContainers = containers
     .sort((a,b) => {
@@ -9,7 +56,6 @@ const sortedContainers = containers
         else return (a.width * a.length < b.width * b.length) ? 1 : -1;
     })
 
-// console.log(sortedContainers)
 
 let placedContainers = new Array(ship.length);
 
@@ -20,24 +66,23 @@ for (let i= 0 ; i< ship.length; i++)
     placedContainers[i][j] = 'x';
 }
 
-//console.log("placed", placedContainers[0])
-//  let x = [[]];
-//  x[0] = [2,1,4];
-//  x[1] = [3,5,5]
-// console.log(x[1][1])
 
-// console.log(placedContainers[0][1])
+// let result = checkPlace(sortedContainers[0], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
 
-if(ship.length >= sortedContainers[0].length){
-    if(ship.width >= sortedContainers[0].width){
-        for(let i=0; i<sortedContainers[0].length; i++)
-            for(let j=0; j<sortedContainers[0].width; j++)
-            placedContainers[i][j] = sortedContainers[0].id;
-    }
-}
+// result = checkPlace(sortedContainers[1], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
 
 
+// result = checkPlace(sortedContainers[2], placedContainers);
+// if(result.valid) placedContainers = result.placedContainers;
 
-const table = cTable.getTable(placedContainers);
+sortedContainers.forEach(el => {
+    let result = checkPlace(el, placedContainers);
+    if(result.valid) placedContainers = result.placedContainers;
+})
+let table = cTable.getTable(placedContainers);
+
 console.table(placedContainers);
 
+console.log(sortedContainers)
