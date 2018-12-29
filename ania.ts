621cce5627510882dd8f5a4bef31585f1324078e
annajjj <ajonak@o2.pl>
2018-12-29 16:08:40 +0100

    mock

---
+++ ania.ts
@@ -0,0 +1,2 @@
+import { containers, ship } from "./mock";
+
---
+++ mock.js
@@ -0,0 +1,134 @@
+"use strict";
+exports.__esModule = true;
+exports.containers = [{
+        id: 1,
+        width: 1,
+        length: 1,
+        timestamp: 1
+    },
+    {
+        id: 2,
+        width: 5,
+        length: 4,
+        timestamp: 1
+    },
+    {
+        id: 3,
+        width: 4,
+        length: 5,
+        timestamp: 2
+    },
+    {
+        id: 4,
+        width: 4,
+        length: 3,
+        timestamp: 1
+    },
+    {
+        id: 5,
+        width: 1,
+        length: 3,
+        timestamp: 1
+    },
+    {
+        id: 6,
+        width: 1,
+        length: 3,
+        timestamp: 2
+    },
+    {
+        id: 7,
+        width: 1,
+        length: 4,
+        timestamp: 2
+    },
+    {
+        id: 8,
+        width: 4,
+        length: 2,
+        timestamp: 3
+    },
+    {
+        id: 9,
+        width: 1,
+        length: 2,
+        timestamp: 1
+    },
+    {
+        id: 10,
+        width: 6,
+        length: 2,
+        timestamp: 1
+    },
+    {
+        id: 11,
+        width: 1,
+        length: 2,
+        timestamp: 2
+    },
+    {
+        id: 12,
+        width: 5,
+        length: 2,
+        timestamp: 1
+    },
+    {
+        id: 13,
+        width: 1,
+        length: 7,
+        timestamp: 1
+    },
+    {
+        id: 14,
+        width: 5,
+        length: 3,
+        timestamp: 1
+    },
+    {
+        id: 15,
+        width: 3,
+        length: 2,
+        timestamp: 1
+    },
+    {
+        id: 16,
+        width: 1,
+        length: 4,
+        timestamp: 1
+    },
+    {
+        id: 17,
+        width: 1,
+        length: 2,
+        timestamp: 2
+    },
+    {
+        id: 18,
+        width: 1,
+        length: 5,
+        timestamp: 1
+    },
+    {
+        id: 19,
+        width: 2,
+        length: 2,
+        timestamp: 1
+    },
+    {
+        id: 20,
+        width: 3,
+        length: 3,
+        timestamp: 1
+    },
+    {
+        id: 21,
+        width: 3,
+        length: 2,
+        timestamp: 4
+    }
+];
+exports.ship = {
+    id: 1,
+    width: 20,
+    lenght: 30
+};
