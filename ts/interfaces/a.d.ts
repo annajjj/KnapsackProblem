f37f75c5e2ea5003ad4d20b1121c5b944fa9a17f
WojciechKasprzyk <w.kasprzyk95@gmail.com>
2019-01-02 17:16:32 +0100

    ship

--- ts/interfaces/a.d.ts
+++ ts/interfaces/a.d.ts
@@ -1,9 +1,18 @@
+
 interface Container {
     id: string;
     width: number;
     length: number;
     height: number;
     timestamp: number;
+}
+
+
+interface Ship{
+    id: string,
+    width: number,
+    length: number,
+    height: number
 }
 
 
