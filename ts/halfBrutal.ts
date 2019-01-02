f62564453314bfd7c9958c62916827b069c4c8d6
annajjj <ajonak@o2.pl>
2019-01-02 19:26:28 +0100

    funtions -> class

--- ts/halfBrutal.ts
+++ ts/halfBrutal.ts
@@ -1,105 +1,38 @@
 import { containers, ship } from "./mock/mock";
 import { generatedContainers } from "./mock/generated-data";
 import * as cTable from 'console.table';
+import { Warehouse } from "./naive";
 
-function checkSpace(container) {
-    let result;
-    for(let floor = 0; floor< floors; floor++){
-        result = checkSpaceXY(container, floor);
-        if (!result.valid) result = checkSpaceXY(container, floor, 'rotated');
-        if (result.valid) break;
+class WarehouseExtended extends Warehouse {
+    //sort containers by timestamp and area
+    sortContainers(containers) {
+        this.sortedContainers = containers
+            .sort((a, b) => {
+                if (a.timestamp > b.timestamp) return 1;
+                else if (a.timestamp < b.timestamp) return -1;
+                else return (a.width * a.length < b.width * b.length) ? 1 : -1;
+            })
     }
-    if (!result.valid) notPlacedContainers.push(container); //dla developementu
-}
 
-function checkSpaceXY(container, floor, type: 'normal' | 'rotated' = 'normal') {
-    if (type == 'rotated') [container.width, container.length] = [container.length, container.width];
-    for (let shipX = 0; shipX < ship.length; shipX++) {
-        for (let shipY = 0; shipY < ship.width; shipY++) {
-            if (placedContainers[`floor${floor}`][shipX][shipY] === 'x') {
-                let checkFloorUnder = 0;
-                for (let containerX = shipX; ((containerX < shipX + container.length) && (shipX + container.length < ship.length + 1)); containerX++) {
-                    for (let containerY = shipY; ((containerY < shipY + container.width) && (shipY + container.width < ship.width + 1)); containerY++) {
-                        if (placedContainers[`floor${floor}`][containerX][containerY] !== 'x') {
-                            containerX = shipX + container.length;
-                            containerY = shipY + container.width;
-                            break;
-                        }
-
-                        if(floor > 0) 
-                          if (placedContainers[`floor${floor-1}`][containerX][containerY] !== 'x') 
-                            checkFloorUnder++;
-
-                        if ((container.length == (containerX - shipX + 1)) && (container.width == (containerY - shipY + 1))) {
-                            if (floor > 0 && checkFloorUnder < (container.length * container.width / 2)) 
-                            {
-                                containerX = shipX + container.length;
-                                containerY = shipY + container.width;
-                                break;
-                            }
-                            fillArray(shipX, shipY, container, floor)
-                            return { valid: true };
-                        }
-                    }
-                }
+    //check space with checkSpaceXY funtion if fails rotate container, if fails2 check other floor
+    checkSpace(container) {
+        let result;
+        for (let floor = 0; floor < this.floors; floor++) {
+            result = this.checkSpaceXY(container, floor);
+            if (!result.valid) {
+                [container.width, container.length] = [container.length, container.width];
+                result = this.checkSpaceXY(container, floor);
             }
+            if (result.valid) break;
         }
-    }
-    return { valid: false };
-}
-
-function fillArray(x, y, container, floor) {
-    for (let i = x; i < container.length + x; i++)
-        for (let j = y; j < container.width + y; j++)
-            placedContainers[`floor${floor}`][i][j] = container.id;
-}
-
-function countFreeSpace(){
-    let freeSpace=0;
-    for (let floor = 0; floor < floors; floor ++){
-        for (let i = 0; i < ship.length; i++) {
-            for (let j = 0; j < ship.width; j++){
-                if(placedContainers[`floor${floor}`][i][j]=="x")
-                freeSpace++;
-            }
-        }
-    }
-    return freeSpace;
-}
-
-function showResults(){
-    let table;
-    for(let floor = 0; floor < floors; floor++){
-        console.log(`floor${floor}`)
-        table = cTable.getTable(placedContainers[`floor${floor}`]);
-        console.table(placedContainers[`floor${floor}`]);
-    }
-    console.log("not placed containers: ", notPlacedContainers);
-}
-
-const sortedContainers = generatedContainers
-    .sort((a, b) => {
-        if (a.timestamp > b.timestamp) return 1;
-        else if (a.timestamp < b.timestamp) return -1;
-        else return (a.width * a.length < b.width * b.length) ? 1 : -1;
-    })
-
-console.time('execution');
-let floors = Math.floor(ship.height / generatedContainers[0].height);
-let placedContainers = {};
-let notPlacedContainers = [];
-
-for (let floor = 0; floor < floors; floor ++){
-    placedContainers[`floor${floor}`] = new Array(ship.length);
-    for (let i = 0; i < ship.length; i++) {
-        placedContainers[`floor${floor}`][i] = new Array(ship.width);
-        for (let j = 0; j < ship.width; j++)
-            placedContainers[`floor${floor}`][i][j] = 'x';
+        if (!result.valid) this.notPlacedContainers.push(container); //dla developementu
     }
 }
 
+export function halfBrutal(ship, containers) {
+    const warehouse = new WarehouseExtended(ship.id, ship.width, ship.length, ship.width, containers);
+    warehouse.placeContainers();
+    warehouse.showResults();
+}
 
-sortedContainers.forEach(el => checkSpace(el));
-// showResults();
-// console.log(notPlacedContainers.length, countFreeSpace())
-console.timeEnd('execution');
+halfBrutal(ship, generatedContainers);
--- ts/naive.ts
+++ ts/naive.ts
@@ -2,103 +2,137 @@ import { containers, ship } from "./mock/mock";
 import { generatedContainers } from "./mock/generated-data";
 import * as cTable from 'console.table';
 
-function checkSpace(container) {
-    let result;
-    for(let floor = 0; floor< floors; floor++){
-        result = checkSpaceXY(container, floor);
-        if (result.valid) break;
+export class Warehouse {
+    id: string;
+    width: number;
+    length: number;
+    height: number;
+    sortedContainers = [];
+    freeSpace: number;
+    floors = Math.floor(ship.height / generatedContainers[0].height);
+    shipSpaceMap = {};
+    notPlacedContainers = [];
+
+    constructor(id, width, length, height, containers) {
+        this.id = id;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+        this.createShipSpaceMap();
+        this.sortContainers(containers);
     }
-    if (!result.valid) notPlacedContainers.push(container); //dla developementu
-}
 
-function checkSpaceXY(container, floor, type: 'normal' | 'rotated' = 'normal') {
-    if (type == 'rotated') [container.width, container.length] = [container.length, container.width];
-    for (let shipX = 0; shipX < ship.length; shipX++) {
-        for (let shipY = 0; shipY < ship.width; shipY++) {
-            if (placedContainers[`floor${floor}`][shipX][shipY] === 'x') {
-                let checkFloorUnder = 0;
-                for (let containerX = shipX; ((containerX < shipX + container.length) && (shipX + container.length < ship.length + 1)); containerX++) {
-                    for (let containerY = shipY; ((containerY < shipY + container.width) && (shipY + container.width < ship.width + 1)); containerY++) {
-                        if (placedContainers[`floor${floor}`][containerX][containerY] !== 'x') {
-                            containerX = shipX + container.length;
-                            containerY = shipY + container.width;
-                            break;
-                        }
+    //create ship space map - floor{i}: [[]]
+    createShipSpaceMap() {
+        for (let floor = 0; floor < this.floors; floor++) {
+            this.shipSpaceMap[`floor${floor}`] = new Array(ship.length);
+            for (let i = 0; i < ship.length; i++) {
+                this.shipSpaceMap[`floor${floor}`][i] = new Array(ship.width);
+                for (let j = 0; j < ship.width; j++)
+                    this.shipSpaceMap[`floor${floor}`][i][j] = 'x';
+            }
+        }
+    }
 
-                        if(floor > 0) 
-                          if (placedContainers[`floor${floor-1}`][containerX][containerY] !== 'x') 
-                            checkFloorUnder++;
+    //sort containers by timestamp and area
+    sortContainers(containers) {
+        this.sortedContainers = containers
+            .sort((a, b) => {
+                if (a.timestamp > b.timestamp) return 1;
+                else if (a.timestamp < b.timestamp) return -1;
+            })
+    }
 
-                        if ((container.length == (containerX - shipX + 1)) && (container.width == (containerY - shipY + 1))) {
-                            if (floor > 0 && checkFloorUnder < (container.length * container.width / 2)) 
-                            {
+    //for each container execute check space method
+    placeContainers() {
+        this.sortedContainers.forEach(el => this.checkSpace(el));
+    }
+
+    //check space with checkSpaceXY funtion if fails2 check other floor
+    checkSpace(container) {
+        let result;
+        for (let floor = 0; floor < this.floors; floor++) {
+            result = this.checkSpaceXY(container, floor);
+            if (result.valid) break;
+        }
+        if (!result.valid) this.notPlacedContainers.push(container); //dla developementu
+    }
+
+    //check if container can be placed
+    checkSpaceXY(container, floor) {
+        for (let shipX = 0; shipX < ship.length; shipX++) {
+            for (let shipY = 0; shipY < ship.width; shipY++) {
+                if (this.shipSpaceMap[`floor${floor}`][shipX][shipY] === 'x') {
+                    let checkFloorUnder = 0;
+                    for (let containerX = shipX; ((containerX < shipX + container.length) && (shipX + container.length < ship.length + 1)); containerX++) {
+                        for (let containerY = shipY; ((containerY < shipY + container.width) && (shipY + container.width < ship.width + 1)); containerY++) {
+                            if (this.shipSpaceMap[`floor${floor}`][containerX][containerY] !== 'x') {
                                 containerX = shipX + container.length;
                                 containerY = shipY + container.width;
                                 break;
                             }
-                            fillArray(shipX, shipY, container, floor)
-                            return { valid: true };
+
+                            if (floor > 0)
+                                if (this.shipSpaceMap[`floor${floor - 1}`][containerX][containerY] !== 'x')
+                                    checkFloorUnder++;
+
+                            if ((container.length == (containerX - shipX + 1)) && (container.width == (containerY - shipY + 1))) {
+                                if (floor > 0 && checkFloorUnder < (container.length * container.width / 2)) {
+                                    containerX = shipX + container.length;
+                                    containerY = shipY + container.width;
+                                    break;
+                                }
+                                this.fillShipSpaceMapWithContainer(shipX, shipY, container, floor)
+                                return { valid: true };
+                            }
                         }
                     }
                 }
             }
         }
+        return { valid: false };
     }
-    return { valid: false };
-}
 
-function fillArray(x, y, container, floor) {
-    for (let i = x; i < container.length + x; i++)
-        for (let j = y; j < container.width + y; j++)
-            placedContainers[`floor${floor}`][i][j] = container.id;
-}
+    //fill space map with container if it can be placed
+    fillShipSpaceMapWithContainer(x, y, container, floor) {
+        for (let i = x; i < container.length + x; i++)
+            for (let j = y; j < container.width + y; j++)
+                this.shipSpaceMap[`floor${floor}`][i][j] = container.id;
+    }
 
-function countFreeSpace(){
-    let freeSpace=0;
-    for (let floor = 0; floor < floors; floor ++){
-        for (let i = 0; i < ship.length; i++) {
-            for (let j = 0; j < ship.width; j++){
-                if(placedContainers[`floor${floor}`][i][j]=="x")
-                freeSpace++;
+    //count in % how much free space is after placemant of containers
+    countFreeSpace() {
+        let freeSpace = 0;
+        for (let floor = 0; floor < this.floors; floor++) {
+            for (let i = 0; i < ship.length; i++) {
+                for (let j = 0; j < ship.width; j++) {
+                    if (this.shipSpaceMap[`floor${floor}`][i][j] == "x")
+                        freeSpace++;
+                }
             }
         }
+        this.freeSpace = freeSpace / (ship.width * ship.length * this.floors);
     }
-    return freeSpace;
+
+    //show results - containers placed in ship, not placed containers and % free space
+    showResults() {
+        let table;
+        for (let floor = 0; floor < this.floors; floor++) {
+            console.log(`floor${floor}`)
+            table = cTable.getTable(this.shipSpaceMap[`floor${floor}`]);
+            console.table(this.shipSpaceMap[`floor${floor}`]);
+        }
+        this.countFreeSpace();
+        console.log("not placed containers: ", this.notPlacedContainers);
+        console.log("free space: ", this.freeSpace);
+    }
+
 }
 
-function showResults(){
-    let table;
-    for(let floor = 0; floor < floors; floor++){
-        console.log(`floor${floor}`)
-        table = cTable.getTable(placedContainers[`floor${floor}`]);
-        console.table(placedContainers[`floor${floor}`]);
-    }
-    console.log("not placed containers: ", notPlacedContainers);
+export function naive(ship, containers) {
+    const warehouse = new Warehouse(ship.id, ship.width, ship.length, ship.width, containers);
+    warehouse.placeContainers();
+    warehouse.showResults();
 }
 
-const sortedContainers = generatedContainers
-    .sort((a, b) => {
-        if (a.timestamp > b.timestamp) return 1;
-        else if (a.timestamp < b.timestamp) return -1;
-    })
-
-console.time('execution');
-
-let floors = Math.floor(ship.height / generatedContainers[0].height);
-let placedContainers = {};
-let notPlacedContainers = [];
-
-for (let floor = 0; floor < floors; floor ++){
-    placedContainers[`floor${floor}`] = new Array(ship.length);
-    for (let i = 0; i < ship.length; i++) {
-        placedContainers[`floor${floor}`][i] = new Array(ship.width);
-        for (let j = 0; j < ship.width; j++)
-            placedContainers[`floor${floor}`][i][j] = 'x';
-    }
-}
-
-
-sortedContainers.forEach(el => checkSpace(el));
-// showResults();
-// console.log(notPlacedContainers.length, countFreeSpace())
-console.timeEnd('execution');
+naive(ship, generatedContainers);
